e4d877655e3434684302c3669a9f338a889c6b95
zainabro753951 <zainabro886@gmail.com>
2026-01-24 20:38:52 +0500

    Some new config

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+console.log("==========================");
+console.log("==========================");
+console.log("==========================");
+console.log("Port: " + process.env.PORT);
+console.log("DB_HOST: " + process.env.DB_HOST);
+console.log("DB_USER: " + process.env.DB_USER);
+console.log("DB_DATABASE: " + process.env.DB_DATABASE);
+console.log("==========================");
+console.log("==========================");
+console.log("==========================");
+
 const server = http.createServer(app);
 
 initSocket(server);
